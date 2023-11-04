644e2fd9ec6e4ed5019c0502fc5aeca0f2bec303
abdullahalarif1 <abdullahrabzil6@gmail.com>
2023-11-04 14:21:03 +0600

    lern upto generic type

--- module2/2.3.ts
+++ module2/2.3.ts
@@ -1,19 +1,42 @@
 {
+  // Generic Type ---------------------------->
 
-// Generic Type
+  // reusable globally handle dynamic generic type --->
+  type GenericArray<T> = Array<T>;
 
+  // const rollNumbers: number[] = [1,2,3]
+  const rollNumbers: GenericArray<number> = [1, 2, 3]; // array generic type
 
+  //const mentors: string[] = ["Mr.x", "Mr. y", "Mr. Z"];
+  const mentors: GenericArray<string> = ["Mr.x", "Mr. y", "Mr. Z"]; // array generic type
 
-const rollNumbers = [1,2,3]
+  //const boolArray: boolean[] = [true, false, true]
+  const boolArray: GenericArray<boolean> = [true, false, true]; // array generic type
 
-const mentors = ['Mr.x', 'Mr. y', 'me']
+  // Array of object for generic ----------------------->
 
+  const user: GenericArray<{ name: string; age: number }> = [
+    {
+      name: "abd",
+      age: 100,
+    },
+    {
+      name: "Jhankar Mahbub",
+      age: 30,
+    },
+  ];
 
+  //generic tuple ------------------------------->
 
+  type GenericTuple<x, y> = [x, y];
 
+  const manush: GenericTuple<string, string> = ["Mr. X", "Mr. Y"];
 
-
-
-
-
+  const UserWithID: GenericTuple<number, { name: string; email: string }> = [
+    23445,
+    {
+      name: "Abdullah",
+      email: "abdullah@gmail.com",
+    },
+  ];
 }
